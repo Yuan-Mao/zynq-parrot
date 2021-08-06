1c77b24e54b97d1ea9f8ba5215d73d55acc73271
Max Ruttenberg <mrutt@cs.washington.edu>
2021-08-05 20:34:34 -0700

    VCS-ified

--- cosim/include/vcs/bp_zynq_pl.h
+++ cosim/include/vcs/bp_zynq_pl.h
@@ -15,8 +15,7 @@
 using namespace std;
 using namespace bsg_nonsynth_dpi;
 
-#include "Vtop.h"
-#include "verilated.h"
+#include <svdpi.h>
 
 #define _STRINGIFY(x) #x
 #define STRINGIFY(x) _STRINGIFY(x)
@@ -84,28 +83,28 @@ using namespace bsg_nonsynth_dpi;
 // W = width of pin
 template <unsigned int W>
 class pin {
-  dpi_gpio<W> *gpio;
+    dpi_gpio<W> *gpio;
 
-  public:
+public:
     pin(const string &hierarchy) {
-      gpio = new dpi_gpio<W>(hierarchy);
+        gpio = new dpi_gpio<W>(hierarchy);
     }
 
     void operator=(const unsigned int val) {
-      unsigned int bval = 0;
-      for (int i = 0; i < W; i++) {
-        bval = (val & (1 << i)) >> i;
-        gpio->set(i, bval);
-      }
+        unsigned int bval = 0;
+        for (int i = 0; i < W; i++) {
+            bval = (val & (1 << i)) >> i;
+            gpio->set(i, bval);
+        }
     }
 
     operator int() const {
-      unsigned int N = 0;
-      for (int i = 0; i < W; i++) {
-        N |= gpio->get(i) << i;
-      }
+        unsigned int N = 0;
+        for (int i = 0; i < W; i++) {
+            N |= gpio->get(i) << i;
+        }
 
-      return N;
+        return N;
     }
 };
 
@@ -113,32 +112,32 @@ class pin {
 // D = axil data width
 template <unsigned int A, unsigned int D>
 class axil {
-  public:
-        pin<1>   p_aclk;
-        pin<1>   p_aresetn;
+public:
+    pin<1>   p_aclk;
+    pin<1>   p_aresetn;
 
-        pin<A>   p_awaddr;
-        pin<3>   p_awprot;
-        pin<1>   p_awvalid;
-        pin<1>   p_awready;
-        pin<D>   p_wdata;
-        pin<D/8> p_wstrb;
-        pin<1>   p_wvalid;
-        pin<1>   p_wready;
-        pin<2>   p_bresp;
-        pin<1>   p_bvalid;
-        pin<1>   p_bready;
+    pin<A>   p_awaddr;
+    pin<3>   p_awprot;
+    pin<1>   p_awvalid;
+    pin<1>   p_awready;
+    pin<D>   p_wdata;
+    pin<D/8> p_wstrb;
+    pin<1>   p_wvalid;
+    pin<1>   p_wready;
+    pin<2>   p_bresp;
+    pin<1>   p_bvalid;
+    pin<1>   p_bready;
 
-        pin<A>   p_araddr;
-        pin<3>   p_arprot;
-        pin<1>   p_arvalid;
-        pin<1>   p_arready;
-        pin<D>   p_rdata;
-        pin<2>   p_rresp;
-        pin<1>   p_rvalid;
-        pin<1>   p_rready;
+    pin<A>   p_araddr;
+    pin<3>   p_arprot;
+    pin<1>   p_arvalid;
+    pin<1>   p_arready;
+    pin<D>   p_rdata;
+    pin<2>   p_rresp;
+    pin<1>   p_rvalid;
+    pin<1>   p_rready;
 
-  axil(const string &base) :
+    axil(const string &base) :
         p_aclk    (string(base) + string(".aclk_gpio")),
         p_aresetn (string(base) + string(".aresetn_gpio")),
         p_awaddr  (string(base) + string(".awaddr_gpio")),
@@ -160,24 +159,27 @@ class axil {
         p_rresp   (string(base) + string(".rresp_gpio")),
         p_rvalid  (string(base) + string(".rvalid_gpio")),
         p_rready  (string(base) + string(".rready_gpio")) {
-            std::cout << base << std::endl;
-        }
+        std::cout << base << std::endl;
+    }
 };
 
 class bp_zynq_pl {
-
-    Vtop *tb;
+    svScope dpi;
+    std::string top_name;
+    std::string dpi_name;
+    std::string full_dpi_name;
+    
     axil<GP0_ADDR_WIDTH,GP0_DATA_WIDTH> *axi_gp0;
     axil<GP1_ADDR_WIDTH,GP1_DATA_WIDTH> *axi_gp1;
 
     // Wait for (low true) reset to be asserted by the testbench
     template <unsigned int A, unsigned int D>
     void reset(axil<A, D> *axil) {
-      printf("bp_zynq_pl: Entering reset\n");
-      while (axil->p_aresetn == 1) {
-        tick();
-      }
-      printf("bp_zynq_pl: Exiting reset\n");
+        printf("bp_zynq_pl: Entering reset\n");
+        while (axil->p_aresetn == 1) {
+            tick();
+        }
+        printf("bp_zynq_pl: Exiting reset\n");
     }
 
     // Each bsg_timekeeper::next() moves to the next clock edge
@@ -186,210 +188,210 @@ class bp_zynq_pl {
     //   the first eval, but BSG designs tend to do assertions on negedge
     //   at the least.
     void tick() {
-      bsg_timekeeper::next();
-      tb->eval();
-      bsg_timekeeper::next();
-      tb->eval();
+        eval();
     }
 
-  public:
+    void eval() {
+        svScope prev;
+        prev = svSetScope(top);
+        bsg_dpi_next();
+        svSetScope(prev);
+    }
+
+public:
   
-  bp_zynq_pl(int argc, char *argv[]) {
-    // Initialize Verilators variables
-    Verilated::commandArgs(argc, argv);
-
-    // turn on tracing
-    Verilated::traceEverOn(true);
-
-    tb = new Vtop;
-
-    // Tick once to register clock generators
-    tb->eval();
-    tick();
+    bp_zynq_pl(int argc, char *argv[]) {
+        top_name = argv[0];
+        dpi_name = argv[1];
+        full_dpi_name = top_name + "." dpi_name;
+        dpi = svGetScopeFromName(full_dpi_name.c_str());
+        
+        // Tick once to register clock generators
+        eval();
+        
+        tick();
 
 #ifdef GP0_ENABLE
-    axi_gp0 = new axil<GP0_ADDR_WIDTH, GP0_DATA_WIDTH>(STRINGIFY(GP0_HIER_BASE));
-    reset(axi_gp0);
+        axi_gp0 = new axil<GP0_ADDR_WIDTH, GP0_DATA_WIDTH>(STRINGIFY(GP0_HIER_BASE));
+        reset(axi_gp0);
 #endif
 #ifdef GP1_ENABLE
-    axi_gp1 = new axil<GP1_ADDR_WIDTH, GP1_DATA_WIDTH>(STRINGIFY(GP1_HIER_BASE));
-    reset(axi_gp1);
+        axi_gp1 = new axil<GP1_ADDR_WIDTH, GP1_DATA_WIDTH>(STRINGIFY(GP1_HIER_BASE));
+        reset(axi_gp1);
 #endif
-  }
-
-  ~bp_zynq_pl(void) {
-    delete tb;
-    tb = NULL;
-  }
-
-  bool done(void) {
-    printf("bp_zynq_pl: done() called, exiting\n");
-    return Verilated::gotFinish();
-  }
-
-  void axil_write(unsigned int address, int data, int wstrb) {
-    int address_orig = address;
-    int index;
-
-    // we subtract the bases to make it consistent with the Zynq AXI IPI implementation
-
-    if (address >=GP0_ADDR_BASE && address <= GP0_ADDR_BASE+GP0_ADDR_SIZE_BYTES)
-    {
-      index = 0;
-      address = address - GP0_ADDR_BASE;
-    }
-    else if (address >= GP1_ADDR_BASE && address <= GP1_ADDR_BASE+GP1_ADDR_SIZE_BYTES)
-    {
-      index = 1;
-      address = address - GP1_ADDR_BASE;
-    }
-    else
-      assert(0);
-
-    if (ZYNQ_PL_DEBUG)
-      printf("  bp_zynq_pl: AXI writing [%x] -> port %d, [%x]<-%8.8x\n", address_orig, index, address, data);
-
-    if (index == 0) {
-      axil_write_helper(axi_gp0, address, data, wstrb);
-    } else if (index == 1) {
-      axil_write_helper(axi_gp1, address, data, wstrb);
-    }
-  }
-
-  template <typename T>
-  void axil_write_helper(T *axil, unsigned int address, int data, int wstrb)
-  {
-    int timeout_counter=0;
-
-    assert(wstrb==0xf); // we only support full int writes right now
-
-    axil->p_awvalid = 1;
-    axil->p_wvalid = 1;
-    axil->p_awaddr = address;
-    axil->p_wdata = data;
-    axil->p_wstrb = wstrb;
-
-    while (axil->p_awready == 0 && axil->p_wready == 0) {
-
-      if (timeout_counter++ > ZYNQ_AXI_TIMEOUT) {
-        printf("bp_zynq_pl: AXI write timeout\n");
-        done();
-        exit(0);
-        assert(0);
-      }
-
-      tick();
     }
 
-    tick();
-
-    // must drop valid signals
-    // let's get things ready with bready at the same time
-    axil->p_awvalid = 0;
-    axil->p_wvalid = 0;
-    axil->p_bready = 1;
-
-    // wait for bvalid to go high
-    while (axil->p_bvalid == 0) {
-      if (timeout_counter++ > ZYNQ_AXI_TIMEOUT) {
-        printf("bp_zynq_pl: AXI bvalid timeout\n");
-        done();
-        exit(0);
-      }
-
-      tick();
+    ~bp_zynq_pl(void) {
+        dpi = nullptr;
     }
 
-    // now, we will drop bready low with ready on the next cycle
-    tick();
-    axil->p_bready  = 0;
-    return;
-  }
-
-
-  int axil_read(unsigned int address) {
-    int address_orig = address;
-    int index = 0;
-    int data;
-
-    // we subtract the bases to make it consistent with the Zynq AXI IPI implementation
-
-    if (address >=GP0_ADDR_BASE && address <= GP0_ADDR_BASE+GP0_ADDR_SIZE_BYTES)
-    {
-      index = 0;
-      address = address - GP0_ADDR_BASE;
-    }
-    else if (address >= GP1_ADDR_BASE && address <= GP1_ADDR_BASE+GP1_ADDR_SIZE_BYTES)
-    {
-      index = 1;
-      address = address - GP1_ADDR_BASE;
-    }
-    else
-      assert(0);
-
-    if (index == 0) {
-      data = axil_read_helper(axi_gp0, address);
-    } else if (index == 1) {
-      data = axil_read_helper(axi_gp1, address);
+    bool done(void) {
+        printf("bp_zynq_pl: done() called, exiting\n");
     }
 
-    if (ZYNQ_PL_DEBUG)
-      printf("  bp_zynq_pl: AXI reading [%x] -> port %d, [%x]->%8.8x\n", address_orig, index, address, data);
+    void axil_write(unsigned int address, int data, int wstrb) {
+        int address_orig = address;
+        int index;
 
-    return data;
-  }
+        // we subtract the bases to make it consistent with the Zynq AXI IPI implementation
 
-  template <typename T>
-  int axil_read_helper(T *axil, unsigned int address) {
-    int data;
-    int timeout_counter = 0;
+        if (address >=GP0_ADDR_BASE && address <= GP0_ADDR_BASE+GP0_ADDR_SIZE_BYTES)
+        {
+            index = 0;
+            address = address - GP0_ADDR_BASE;
+        }
+        else if (address >= GP1_ADDR_BASE && address <= GP1_ADDR_BASE+GP1_ADDR_SIZE_BYTES)
+        {
+            index = 1;
+            address = address - GP1_ADDR_BASE;
+        }
+        else
+            assert(0);
 
-    // assert these signals "late in the cycle"
-    axil->p_arvalid = 1;
-    axil->p_araddr = address;
+        if (ZYNQ_PL_DEBUG)
+            printf("  bp_zynq_pl: AXI writing [%x] -> port %d, [%x]<-%8.8x\n", address_orig, index, address, data);
 
-    // stall while ready is not asserted
-    while (axil->p_arready == 0)
-      {
-        if (timeout_counter++ > ZYNQ_AXI_TIMEOUT) {
-          printf("bp_zynq_pl: AXI read arready timeout\n");
-          done();
-          exit(0);
+        if (index == 0) {
+            axil_write_helper(axi_gp0, address, data, wstrb);
+        } else if (index == 1) {
+            axil_write_helper(axi_gp1, address, data, wstrb);
+        }
+    }
+
+    template <typename T>
+    void axil_write_helper(T *axil, unsigned int address, int data, int wstrb)
+        {
+            int timeout_counter=0;
+
+            assert(wstrb==0xf); // we only support full int writes right now
+
+            axil->p_awvalid = 1;
+            axil->p_wvalid = 1;
+            axil->p_awaddr = address;
+            axil->p_wdata = data;
+            axil->p_wstrb = wstrb;
+
+            while (axil->p_awready == 0 && axil->p_wready == 0) {
+
+                if (timeout_counter++ > ZYNQ_AXI_TIMEOUT) {
+                    printf("bp_zynq_pl: AXI write timeout\n");
+                    done();
+                    exit(0);
+                    assert(0);
+                }
+
+                tick();
+            }
+
+            tick();
+
+            // must drop valid signals
+            // let's get things ready with bready at the same time
+            axil->p_awvalid = 0;
+            axil->p_wvalid = 0;
+            axil->p_bready = 1;
+
+            // wait for bvalid to go high
+            while (axil->p_bvalid == 0) {
+                if (timeout_counter++ > ZYNQ_AXI_TIMEOUT) {
+                    printf("bp_zynq_pl: AXI bvalid timeout\n");
+                    done();
+                    exit(0);
+                }
+
+                tick();
+            }
+
+            // now, we will drop bready low with ready on the next cycle
+            tick();
+            axil->p_bready  = 0;
+            return;
         }
 
-        tick();
-      }
 
-    // ready was asserted, transaction will be accepted!
-    tick();
+    int axil_read(unsigned int address) {
+        int address_orig = address;
+        int index = 0;
+        int data;
 
-    // arvalid must drop the request
-    axil->p_arvalid = 0;
+        // we subtract the bases to make it consistent with the Zynq AXI IPI implementation
 
-    // setup to receive the reply
-    axil->p_rready  = 1;
+        if (address >=GP0_ADDR_BASE && address <= GP0_ADDR_BASE+GP0_ADDR_SIZE_BYTES)
+        {
+            index = 0;
+            address = address - GP0_ADDR_BASE;
+        }
+        else if (address >= GP1_ADDR_BASE && address <= GP1_ADDR_BASE+GP1_ADDR_SIZE_BYTES)
+        {
+            index = 1;
+            address = address - GP1_ADDR_BASE;
+        }
+        else
+            assert(0);
 
-    // stall while valid is not asserted
-    while (axil->p_rvalid == 0)
-      {
-        if (timeout_counter++ > ZYNQ_AXI_TIMEOUT) {
-          printf("bp_zynq_pl: AXI read rvalid timeout\n");
-          done();
-          exit(0);
+        if (index == 0) {
+            data = axil_read_helper(axi_gp0, address);
+        } else if (index == 1) {
+            data = axil_read_helper(axi_gp1, address);
         }
 
+        if (ZYNQ_PL_DEBUG)
+            printf("  bp_zynq_pl: AXI reading [%x] -> port %d, [%x]->%8.8x\n", address_orig, index, address, data);
+
+        return data;
+    }
+
+    template <typename T>
+    int axil_read_helper(T *axil, unsigned int address) {
+        int data;
+        int timeout_counter = 0;
+
+        // assert these signals "late in the cycle"
+        axil->p_arvalid = 1;
+        axil->p_araddr = address;
+
+        // stall while ready is not asserted
+        while (axil->p_arready == 0)
+        {
+            if (timeout_counter++ > ZYNQ_AXI_TIMEOUT) {
+                printf("bp_zynq_pl: AXI read arready timeout\n");
+                done();
+                exit(0);
+            }
+
+            tick();
+        }
+
+        // ready was asserted, transaction will be accepted!
         tick();
-      }
 
-    // if valid was asserted, latch the incoming data
-    data = axil->p_rdata;
-    tick();
+        // arvalid must drop the request
+        axil->p_arvalid = 0;
 
-    // drop the ready signal on the following cycle
-    axil->p_rready  = 0;
+        // setup to receive the reply
+        axil->p_rready  = 1;
 
-    return data;
-  }
+        // stall while valid is not asserted
+        while (axil->p_rvalid == 0)
+        {
+            if (timeout_counter++ > ZYNQ_AXI_TIMEOUT) {
+                printf("bp_zynq_pl: AXI read rvalid timeout\n");
+                done();
+                exit(0);
+            }
+
+            tick();
+        }
+
+        // if valid was asserted, latch the incoming data
+        data = axil->p_rdata;
+        tick();
+
+        // drop the ready signal on the following cycle
+        axil->p_rready  = 0;
+
+        return data;
+    }
 };
 
 #endif
